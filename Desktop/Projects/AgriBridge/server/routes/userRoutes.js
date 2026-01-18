1eead2e3ff5fc75a9bad80bde779adedd27abeb1
Sumit Jaiswal <146453246+sumitjaiswal55@users.noreply.github.com>
2026-01-18 12:44:22 +0530

    Delete Desktop directory

--- Desktop/Projects/AgriBridge/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {registerUser, loginUser} =  require("../controller/authController")
-
-
-router.post("/signup", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
--- Desktop/Wonderlust/utils/wrapAsync.js
+++
@@ -1,6 +0,0 @@
-// wrapAsync.js
-const wrapAsync = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = wrapAsync;
